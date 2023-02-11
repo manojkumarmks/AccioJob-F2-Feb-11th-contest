1447a74b223e513f774bc89ba09e58260596ecc5
Manoj Kumar Sahoo <112025244+manojkumarmks@users.noreply.github.com>
2023-02-11 21:41:23 +0530

    final commit

--- script.js
+++ script.js
@@ -6,44 +6,34 @@ let arr = [
   { id: 3, name: "karen", age: "19", marks: 35 },
 ];
 
-function PrintStudentswithMap() {
-  arr.map(student => {
-    if (student.marks >= 50) {
-      console.log(student);
-    }
-  });
+function PrintStudentbyMap() {
+  arr.map(student => student.marks >= 50 && console.log(student));
 }
 
-function PrintStudentsbyForEach() {
-  arr.forEach(student => {
-    if (student.marks >= 50) {
-      console.log(student);
-    }
-  });
+function PrintStudentbyForEach() {
+  arr.forEach(student => student.marks >= 50 && console.log(student));
 }
 
 function addData() {
-  const newStudent = { id: 4, name: "susan", age: "20", marks: 45 };
-  arr.push(newStudent);
-  console.log(newStudent);
+  arr.push({ id: 4, name: "susan", age: "20", marks: 45 });
+  console.log(arr[arr.length - 1]);
 }
 
 function removeFailedStudent() {
-  const updatedArray = arr.filter(student => student.marks >= 50);
-  console.log(updatedArray);
+  arr = arr.filter(student => student.marks >= 50);
+  console.log(arr);
 }
 
-function concatenateArray() {
-  const newArray = [
+function ConcatenateArray() {
+  arr = arr.concat([
     { id: 5, name: "Alex", age: "19", marks: 65 },
     { id: 6, name: "Jane", age: "20", marks: 80 },
     { id: 7, name: "Tom", age: "18", marks: 90 }
-  ];
-  arr = arr.concat(newArray);
+  ]);
   console.log(arr);
 }
-PrintStudentswithMap();
-PrintStudentsbyForEach();
+PrintStudentbyMap();
+PrintStudentbyForEach();
 addData();
 removeFailedStudent();
-concatenateArray();
+ConcatenateArray();
